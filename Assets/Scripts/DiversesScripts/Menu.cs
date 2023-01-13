bb0833762692f935c1f1d805f03e6a02734d81c8
Laetus-Ajax <86963641+Laetus-Ajax@users.noreply.github.com>
2023-01-13 13:22:12 +0100

    Fix the previous bug and added placeholder on Menu for Character Selection
    The next step is to configure the choice of the player, and choose the appropriate sprite.

--- Assets/Scripts/DiversesScripts/Menu.cs
+++ Assets/Scripts/DiversesScripts/Menu.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    public int SpriteP1;
-    public int SpriteP2;
+    public int spriteP1;
+    public int spriteP2;
+
+    public Image p1, p2;
+
+    public Sprite[] _sprites;
     
     public void StartButton()
     {
@@ -15,17 +20,19 @@ public class Menu : MonoBehaviour
 
     public void selectPersoP1(int dir)
     {
-        SpriteP1 += dir;
-        if (SpriteP1 > 3) SpriteP1 = 1;
-        if (SpriteP1 < 1) SpriteP1 = 3;
-        print(SpriteP1);
+        spriteP1 += dir;
+        if (spriteP1 > 3) spriteP1 = 1;
+        if (spriteP1 < 1) spriteP1 = 3;
+        p1.sprite = _sprites[spriteP1 -1];
+        print(spriteP1);
     }
 
     public void selectPersoP2(int dir2)
     {
-        SpriteP2 += dir2;
-        if (SpriteP2 > 3) SpriteP2 = 1;
-        if (SpriteP2 < 1) SpriteP2 = 3;
-        print(SpriteP2);
+        spriteP2 += dir2;
+        if (spriteP2 > 3) spriteP2 = 1;
+        if (spriteP2 < 1) spriteP2 = 3;
+        p2.sprite = _sprites[spriteP2 -1];
+        print(spriteP2);
     }
 }
