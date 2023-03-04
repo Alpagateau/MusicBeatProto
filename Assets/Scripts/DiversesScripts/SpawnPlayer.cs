46c617cd238133bf40be011d298cdfb2d8d147af
Alpa <52045495+Alpagateau@users.noreply.github.com>
2023-03-04 21:50:11 +0100

    Resolved - 1
    resolved problems

--- Assets/Scripts/DiversesScripts/SpawnPlayer.cs
+++ Assets/Scripts/DiversesScripts/SpawnPlayer.cs
@@ -25,20 +25,18 @@ public class SpawnPlayer : MonoBehaviour
             GameObject Player1 = GameObject.Instantiate(playerTypes[ke.p1 - 1], p1Position, Quaternion.identity);
             GameObject Player2 = GameObject.Instantiate(playerTypes[ke.p2 - 1], p2Position, Quaternion.identity);
 
-<<<<<<< Updated upstream
-        player p1 = Player1.GetComponent<player>();
-        player p2 = Player2.GetComponent<player>();
-        healthControl HealthP1 = Player1.GetComponent<healthControl>();
-        healthControl HealthP2 = Player2.GetComponent<healthControl>();
-        p1.keyCodes = Player1Properties.keyCodes;
-        p2.keyCodes = Player2Properties.keyCodes;
-        p1.otherPlayer = p2;
-        p2.otherPlayer = p1;
-        p1.isLookingRight = true;
-        p2.isLookingRight = false;
-        HealthP1.HealthBar = HealthBarP1;
-        HealthP2.HealthBar = HealthBarP2;
-=======
+            player p1 = Player1.GetComponent<player>();
+            player p2 = Player2.GetComponent<player>();
+            healthControl HealthP1 = Player1.GetComponent<healthControl>();
+            healthControl HealthP2 = Player2.GetComponent<healthControl>();
+            p1.keyCodes = Player1Properties.keyCodes;
+            p2.keyCodes = Player2Properties.keyCodes;
+            p1.otherPlayer = p2;
+            p2.otherPlayer = p1;
+            p1.isLookingRight = true;
+            p2.isLookingRight = false;
+            HealthP1.HealthBar = HealthBarP1;
+            HealthP2.HealthBar = HealthBarP2;
             Player p1 = Player1.GetComponent<Player>();
             Player p2 = Player2.GetComponent<Player>();
             /*Ordi Ordi1 = Player1.GetComponent<Ordi>();
@@ -62,6 +60,6 @@ public class SpawnPlayer : MonoBehaviour
             Ordi2.mouse = mouse;
             Ordi2.mouseCable = cable;
         }*/
->>>>>>> Stashed changes
+
     }
 }
