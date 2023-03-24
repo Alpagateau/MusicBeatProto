344f0401b06748643cf701b6f5fca89a875de23f
l0bst3rr0r <davcoz31@gmail.com>
2023-03-24 16:01:31 +0100

    [CHANGELOG] -MainGame:  * Added a smooth animation when the health bar value decreases + changes color when it hits a certain value (green:100-50,orange:50-25,red:25-0)  * Added a pause menu when hitting the ESCAPE key (Note: I'm still working on the settings menu)

--- Assets/Scripts/HealthBar/healthControl.cs
+++ Assets/Scripts/HealthBar/healthControl.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +6,50 @@ using UnityEngine.UI;
 
 public class healthControl : MonoBehaviour
 {
+    public float damageAnimationDuration = 0.5f;
     public Image HealthBar;
     public float maxHP = 100f;
     public float HP;
-
+    Color orange = new Color32(255, 255, 0, 100);
+    Color green = new Color32(0,255,0,100);
+    Color red = new Color32(255, 0, 0, 100);
     public void Start()
     {
         HP = maxHP;
+        HealthBar.color = green;
+
     }
 
     public void TakeDamage(int dmg)
     {
         HP -= dmg;
-        HealthBar.fillAmount = HP / maxHP;
-    }
+        float fillAmount = HP / maxHP;
+        float targetFillAmount = fillAmount - 0.1f;
+        StartCoroutine(LerpFillAmount(targetFillAmount, damageAnimationDuration));
+        if (HP <= 50)
+        {
+            HealthBar.color = orange;
+        }
+        if (HP <= 25)
+        {
+            HealthBar.color = red;
 
+        }
+    }
+    private IEnumerator LerpFillAmount(float targetFillAmount, float duration)
+    {
+        float startTime = Time.time;
+        float startFillAmount = HealthBar.fillAmount;
+
+        while (Time.time < startTime + duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            HealthBar.fillAmount = Mathf.Lerp(startFillAmount, targetFillAmount, t);
+            yield return null;
+        }
+
+        HealthBar.fillAmount = targetFillAmount;
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
         TakeDamage(10);
